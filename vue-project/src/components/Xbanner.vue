a0f40742f17761e5ce27e08c800449121fce0f4d
jieke <13437965971@163.com>
2019-01-23 16:26:43 +0800

    轮播图

--- vue-project/src/components/Xbanner.vue
+++ vue-project/src/components/Xbanner.vue
@@ -1,17 +1,35 @@
 <template>
-    <img src="../assets/banner1.jpeg" />
+  <el-carousel height="150px" :interval="5000" arrow="always">
+    <el-carousel-item v-for="(item,index) in imglist" :key="index">
+        <img :src="item" alt="">
+    </el-carousel-item>
+  </el-carousel>
 </template>
 <script>
-//引入图片
-import '../assets/banner1.jpeg'
+import img1 from '../assets/149482.jpeg';
+import img2 from '../assets/149716.jpeg';
+import img3 from '../assets/149817.jpeg';
+import img4 from '../assets/banner1.jpeg';
 export default {
-    
+    data(){
+        return {
+            imglist:[img1,img2,img3,img4]
+        }
+    }
 }
 </script>
-<style scoped>
-    img{
-        width:400px;
-        height: 200px;
-    }
-</style>
 
+<style scoped>
+  .el-carousel__item img {
+    width:100%;
+    height: 150px;
+  }
+  
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+    background-color: #d3dce6;
+  }
+</style>
